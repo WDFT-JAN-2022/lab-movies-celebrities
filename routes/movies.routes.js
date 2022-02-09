a08f8a0f3b99293d05cc82e192daf007738be9cf
yeatsg <yeatsg@gmail.com>
2022-02-09 10:12:29 -0500

    yeats

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,32 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Movie = require("../models/Movie.model");
+const Celebrity = require("../models/Celebrity.model");
 
 // all your routes here
+
+router.get("/create", (req, res) => {
+  Celebrity.find()
+    .then((allCelebs) => {
+      res.render("../views/movies/new-movie", {
+        celebrities: allCelebs,
+      });
+    })
+    .catch((err) => {
+      console.log("Something went wrong", err);
+    });
+});
+
+router.post("/create", (req, res) => {
+  const movie = req.body;
+  Movie.create(movie)
+    .then((newMovie) => {
+      console.log("Creation was  succesful", newMovie);
+      res.redirect("/movies/all-movies");
+    })
+    .catch((err) => {
+      console.log("Something went wrong", err);
+    });
+});
 
 module.exports = router;
